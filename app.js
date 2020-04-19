caf1d1d8c016f8d6c93f82c05a2c8345c069a897
nivaner <yangzhiqiang0118@gmail.com>
2020-04-20 07:59:06 +0800

    add login moudle

--- app.js
+++ app.js
@@ -34,6 +34,63 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    version: "1.0",
+    shopName: "大象洗鞋",
+    domain: "http://127.0.0.1:8999/api"
+  },
+  buildUrl: function (path, params) {
+    var url = this.globalData.domain + path;
+    var _paramUrl = "";
+    if (params) {
+      _paramUrl = Object.keys(params).map(function (k) {
+        return [encodeURIComponent(k), encodeURIComponent(params[k])].join("=");
+      }).join("&");
+      _paramUrl = "?" + _paramUrl;
+    }
+    return url + _paramUrl;
+  }, 
+  getRequestHeader: function () {
+    return {
+      'content-type': 'application/x-www-form-urlencoded',
+      // 'Authorization': this.getCache("token")
+    }
+  },
+  alert: function (params) {
+    var title = params.hasOwnProperty('title') ? params['title'] : '大象洗鞋提示您';
+    var content = params.hasOwnProperty('content') ? params['content'] : '';
+    wx.showModal({
+      title: title,
+      content: content,
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {//用户点击确定
+          if (params.hasOwnProperty('cb_confirm') && typeof (params.cb_confirm) == "function") {
+            params.cb_confirm();
+          }
+        } else {
+          if (params.hasOwnProperty('cb_cancel') && typeof (params.cb_cancel) == "function") {
+            params.cb_cancel();
+          }
+        }
+      }
+    })
+  },
+  console: function (msg) {
+    console.log(msg);
+  },
+  getCache: function (key) {
+    var value = undefined;
+    try {
+      value = wx.getStorageSync(key);
+    } catch (e) {
+    }
+    return value;
+  },
+  setCache: function (key, value) {
+    wx.setStorage({
+      key: key,
+      data: value
+    });
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,15 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    remind: '加载中',
+    angle: 0,
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    regFlag: true
+  },
+  goToIndex: function () {
+    wx.switchTab({
+      url: '/pages/food/food',
+    });
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,39 +21,66 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    this.checkLogin();
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  checkLogin: function () {
+    var that = this;
+    wx.login({
+      success: function (res) {
+        if (!res.code) {
+          app.alert({ 'content': '登录失败，请再次点击~~' });
+          return;
+        }
+        wx.request({
+          url: app.buildUrl('/member/check-reg'),
+          header: app.getRequestHeader(),
+          method: 'POST',
+          data: { code: res.code },
+          success: function (res) {
+            if (res.data.code != 200) {
+              that.setData({
+                regFlag: false
+              });
+              return;
+            }
+
+            app.setCache("token", res.data.data.token);
+            //that.goToIndex();
+          }
+        });
+      }
+    });
+  },
+  login: function (e) {
+    var that = this;
+    if (!e.detail.userInfo) {
+      app.alert({ 'content': '登录失败，请再次点击~~' });
+      return;
+    }
+
+    var data = e.detail.userInfo;
+    wx.login({
+      success: function (res) {
+        if (!res.code) {
+          app.alert({ 'content': '登录失败，请再次点击~~' });
+          return;
+        }
+        data['code'] = res.code;
+        wx.request({
+          url: app.buildUrl('/member/login'),
+          header: app.getRequestHeader(),
+          method: 'POST',
+          data: data,
+          success: function (res) {
+            if (res.data.code != 200) {
+              app.alert({ 'content': res.data.msg });
+              return;
+            }
+            app.setCache("token", res.data.data.token);
+            that.goToIndex();
+          }
+        });
+      }
+    });
   }
 })
